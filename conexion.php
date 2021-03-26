f2522da5fb8cad43cc22d0651c7fa20dbbefbae7
Julio <jcarias0691@gmail.com>
2021-03-26 13:11:56 -0600

    login

--- conexion.php
+++ conexion.php
@@ -1,17 +1,19 @@
-<?php 
-$host = "localhost";
-$user = "root";
-$clave = "Diosesamor.20";
-$bd = "sis_venta";
+<?php
 
-$conexion = mysqli_connect($host,$user,$clave,$bd);
-if (mysqli_connect_errno()) {
-    echo "No se pudo conectar a la base de datos";
-    exit();
-}
+    $host = "localhost";
+    $user = "root";
+    $clave = "Diosesamor.20";
+    $bd = "sis_venta";
 
-mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
+    $conexion = mysqli_connect($host,$user,$clave,$bd);
+    if (mysqli_connect_errno()){
+        echo "No se pudo conectar a la base de datos";
+        exit();
+    }
 
-mysqli_set_charset($conexion,"utf8");
+    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
+
+    mysqli_set_charset($conexion,"utf8");
+
 
 ?>
